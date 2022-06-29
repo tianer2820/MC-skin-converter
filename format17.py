b14f86bec48f3f7846f7609f7c3f9dcf932a4188
Toby Yang <tianer2820@163.com>
2022-06-29 12:20:04 -0400

    format documents

--- format17.py
+++ format17.py
@@ -4,11 +4,11 @@ import os
 import sys
 from PIL import Image
 
-try :
-	arg = sys.argv[1]
-except IndexError :
-	print("Usage: format17.py <file|dir>")
-	sys.exit(1)
+try:
+    arg = sys.argv[1]
+except IndexError:
+    print("Usage: format17.py <file|dir>")
+    sys.exit(1)
 
 skins = [arg]
 success = 0
@@ -16,26 +16,27 @@ failure = 0
 path = ""
 pl = "s"
 
-if os.path.isdir(arg) :
-	path = arg
-	skins = os.listdir(arg)
+if os.path.isdir(arg):
+    path = arg
+    skins = os.listdir(arg)
 
-for sfile in skins :
-	sfile = os.path.join(path, sfile)
-	try :
-		imi = Image.open(sfile)
-	except IOError :
-		failure += 1
-	else :
-		w, h = imi.size
-		if h == w and h >= 64 :
-			imo = Image.new("RGBA", (w, int(h / 2)))
-			box = (0, 0, w, int(h / 2))
-			imo.paste(imi.crop(box), box)
-			imo.save(sfile)
-			success += 1
-		else :
-			failure += 1
+for sfile in skins:
+    sfile = os.path.join(path, sfile)
+    try:
+        imi = Image.open(sfile)
+    except IOError:
+        failure += 1
+    else:
+        w, h = imi.size
+        if h == w and h >= 64:
+            imo = Image.new("RGBA", (w, int(h / 2)))
+            box = (0, 0, w, int(h / 2))
+            imo.paste(imi.crop(box), box)
+            imo.save(sfile)
+            success += 1
+        else:
+            failure += 1
 
-if success == 1 : pl = ""
+if success == 1:
+    pl = ""
 print("%i skin%s successfully converted, %i failed" % (success, pl, failure))
